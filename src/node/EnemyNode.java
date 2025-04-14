94bbfaed9609dd7d8cf3ff4758e0493466a280cc
Azther0z <saintvsboy@gmail.com>
2025-04-14 12:05:27 +0700

    Link with SceneManager

--- src/node/EnemyNode.java
+++ src/node/EnemyNode.java
@@ -5,12 +5,10 @@ public class EnemyNode extends Node {
 
 	public EnemyNode(int row, int col) {
 		super(row, col, "Enemy");
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void executeNode() {
-		// TODO Auto-generated method stub
 		SceneManager.getInstance().goToFight();
 	}
 
