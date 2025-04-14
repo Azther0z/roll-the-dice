ece78e68360f96f0dad37b9b295c9efe1f9466f7
Azther0z <saintvsboy@gmail.com>
2025-04-14 12:03:21 +0700

    Swap row, col of node. Add other menu on top of MapMenu

--- src/gui/MapMenu.java
+++ src/gui/MapMenu.java
@@ -1,8 +1,13 @@
 package gui;
 
+import dice.Dice;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import node.Node;
 import utils.GameConfig;
 import utils.GameLogic;
@@ -15,20 +20,35 @@ public class MapMenu extends StackPane {
 		for (int i = 0; i < GameConfig.MAX_ROW; i++) {
 			for (int j = 0; j < GameConfig.MAX_COL; j++) {
 				Node node = GameLogic.getInstance().getNodeGrid().get(i).get(j);
-				nodePane.add(node, i, j);
+				nodePane.add(node, j, i);
 			}
 		}
 		nodePane.setAlignment(Pos.CENTER);
 		this.getChildren().add(nodePane);
 	}
-	// TODO RewardMenu
-	
-	// TODO RestMenu
-	
-	// TODO ShopMenu
-	
+
+	public Button createContinueButton() {
+		Button continueButton = new Button("Continue");
+		continueButton.setOnMouseClicked(e -> {
+			this.getChildren().removeLast();
+		});
+		return continueButton;
+	}
+
+	public void initializeRewardMenu() {
+		this.getChildren().add(new RewardMenu());
+	}
+
+	public void initializeRestMenu() {
+		this.getChildren().add(new RestMenu());
+	}
+
+	public void initializeShopMenu() {
+		this.getChildren().add(new ShopMenu());
+	}
+
 	// TODO WinMenu
-	
+
 	// TODO LoseMenu
 
 }
