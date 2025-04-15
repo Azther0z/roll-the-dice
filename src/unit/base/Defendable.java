8f71960b156b0d490baa29ca31f336a16a7fd0eb
Azther0z <saintvsboy@gmail.com>
2025-04-15 13:18:25 +0700

    Remove TODO (usable design, no fix need)

--- src/unit/base/Defendable.java
+++ src/unit/base/Defendable.java
@@ -2,8 +2,6 @@ package unit.base;
 
 public interface Defendable {
 	public void updateDefend();
-	// TODO can make this interface more rational by making
-	// defendable unit defend other unit
 	
 	public int getDefVal();
 }
